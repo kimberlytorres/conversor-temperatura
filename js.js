1f1b275af240c3042b128b669f0b27a78db25564
Kimberly Torres <kimberly.torres.mattos@gmail.com>
2021-10-13 15:56:22 -0300

    criando o arquivo js

---
+++ js.js
@@ -0,0 +1,7 @@
+function ConverterParaCelsius() {
+    var valorEmFahrenheit = parseInt(document.getElementById("valor").value);
+    var valorEmCelsius = ((valorEmFahrenheit - 32) / 1.8);
+    var valorConvertido = document.getElementById("convertidoEmCelsius");
+    var resultado = "A temperatura em graus Celsius é " + valorEmCelsius + ".";
+    valorConvertido.innerHTML = resultado;
+}
